59a2463e2f3fe83e829caa06442315c8d8f8077e
AaronPBishop <aaronpbishop@gmail.com>
2022-06-11 18:47:54 -0500

    numIslands() solved

--- counting_island.js
+++ counting_island.js
@@ -3,7 +3,9 @@
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 */
 
-const grid1 = Input: grid = [
+
+
+const grid1 = [
     ["1","1","1","1","0"],
     ["1","1","0","1","0"],
     ["1","1","0","0","0"],
@@ -11,7 +13,7 @@ const grid1 = Input: grid = [
   ] // 1
 
 
-  const grid2 = grid = [
+const grid2 = [
     ["1","1","0","0","0"],
     ["1","1","0","0","0"],
     ["0","0","1","0","0"],
@@ -28,18 +30,58 @@ grid[i][j] is '0' or '1'.
 */
 
 
-const getNeighbors  = (row, col, grid) {
+const getNeighbors  = (row, col, grid) => {
+    const neighbors = [
+        [row - 1, col],  // Up
+        [row + 1, col], // Down
+        [row, col - 1], // Left
+        [row, col + 1], // Right
+    ];
 
-    const neighbors =
+    const validNeighbors = neighbors.filter((neighbor) => {
+        const [innerRow, innerColumn] = neighbor;
 
-}
+        return grid[innerRow] && grid[innerRow][innerColumn] && grid[innerRow][innerColumn] === "1";
+    });
+    
+    return validNeighbors;
+};
 
 
 const numIslands = function(grid) {
+    // Initialize a set to check values that we have already iterated through
     const visited = new Set();
-    for (let row = 0; row < grid.length; row++){
-        for (let col = 0; col < grid[row].length; col++){
-            let startingNode = grid[row][col];
-        }
-    }
+    // Initialize a count
+    let count = 0;
+    // Iterate through all indices in matrix to build index values
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            // If the node value meets criteria, pass that node forward
+            if (grid[row][col] === "1" && !visited.has([row, col].toString())) {
+                count++;
+
+                visited.add([row, col].toString());
+
+                const stack = [[row, col]];
+                while (stack.length > 0) {
+                    let currentNode = stack.pop();
+                    let [currentRow, currentColumn] = currentNode;
+
+                    const neighbors = getNeighbors(currentRow, currentColumn, grid);
+                    neighbors.forEach((neighbor) => {
+                        let neighborString = neighbor.toString();
+                        if (!visited.has(neighborString)) {
+                            visited.add(neighborString);
+                            stack.push(neighbor);
+                        };
+                    });
+                };
+            };
+        };
+    };
+
+    return count;
 };
+
+console.log(numIslands(grid1))
+console.log(numIslands(grid2))
